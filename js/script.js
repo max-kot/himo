70af26a172daa566f59f1e5c8bf5c0e77f96aee8
sim-max <101747552+sim-max@users.noreply.github.com>
2022-07-15 19:35:04 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+	$('.first-slider').slick({
+		dots: true,
+		arrows: false,
+		fade: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		draggable: true,
+	})
+})
